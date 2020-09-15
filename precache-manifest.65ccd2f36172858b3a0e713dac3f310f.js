83b97fbc785b8541aeeb6b3a88d952cad914ba75
JB-Tellez <jb.tellez@gmail.com>
2020-09-15 08:26:27 +0000

    Deploying to gh-pages from  @ 70677b943dadf3bcf9a28b45dd12df5f2ad7a225 🚀

---
+++ precache-manifest.65ccd2f36172858b3a0e713dac3f310f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88b411519d8957d0bff85274897d68b",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "b7cc52b8edbabd95ef25",
+    "url": "/resty/static/css/main.6ae4e22d.chunk.css"
+  },
+  {
+    "revision": "3cd639d2c27975eba4cf",
+    "url": "/resty/static/js/2.1c5dabfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.1c5dabfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7cc52b8edbabd95ef25",
+    "url": "/resty/static/js/main.eb27344c.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
