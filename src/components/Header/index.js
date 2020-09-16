5ca2c369c9690be5bbb77f5b4ed0d1b75ffc021b
JB-Tellez <jb.tellez@gmail.com>
2020-09-16 16:48:43 -0700

    resty is the best text change

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import './Header.scss';
 export default function Header() {
     return (
         <header className="App-header">
-            <h1>ReSTy</h1>
+            <h1>ReSTy is the Best</h1>
         </header>
     )
 }
