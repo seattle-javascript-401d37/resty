7f83515d9d8051586f03e2d24a0f749979146d3f
JB-Tellez <jb.tellez@gmail.com>
2020-09-17 11:13:29 -0700

    adds final code

--- src/components/history-sidebar.js
+++ src/components/history-sidebar.js
@@ -1,20 +1,19 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 function History(props) {
 
-  const calls = props.calls || {};
-
   function loadRequest(apiCall) {
     props.handler(apiCall);
+    props.history.push('/');
   }
 
   return (
     <aside className="history">
       <ul>
         {
-          Object.keys(calls).map(key =>
+          Object.keys(props.calls).map(key =>
             <li key={key}>
-
               <span className={`method ${props.calls[key].method}`}>{props.calls[key].method}</span>
               <button className="url" onClick={() => loadRequest(props.calls[key])}>{props.calls[key].url}</button>
             </li>,
@@ -25,4 +24,4 @@ function History(props) {
   );
 }
 
-export default History;
+export default withRouter(History);
